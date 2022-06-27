a5a12b5ee964153ff4007103401a6cf22b4eb0a3
NickRTR <hae1crw0qnd-fkm8ZAE>
2022-06-27 20:54:44 +0200

    Initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,13 @@
+<script>
+    import autoselect from "$lib/autoselect.js";
+
+    let example = "Hello World!";
+</script>
+
+<h1>svelte-autoselect</h1>
+
+<p>Without autoselect:</p>
+<input type="text" bind:value={example}>
+
+<p>With autoselect:</p>
+<input type="text" bind:value={example} use:autoselect>
